feecc0794b3d8eb924a503c47d2bcde7cebff9cc
lewagongiuliano-stack <lewagongiuliano@gmail.com>
2025-12-02 14:26:26 +0100

    Create explorationGoodreads.sql

---
+++ explorationGoodreads.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE TABLE
+    `source_data.goodreads_ratings_CLEANED` -- Choose a new, distinct table name
+AS
+SELECT
+    bookID,
+    title,
+    authors,
+    SAFE_CAST(average_rating AS FLOAT64) AS average_rating, 
+    isbn,
+    isbn13,
+    language_code,
+    CAST(ratings_count AS INT64) AS ratings_count,
+    text_reviews_count,
+    publication_date,
+    publisher
+FROM
+    `source_data.goodreads_ratings`
+WHERE
+    SAFE_CAST(average_rating AS FLOAT64) IS NOT NULL;
