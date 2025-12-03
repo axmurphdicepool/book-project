a50f077c7b073dd8f18ff4bc523dbf1a710361b6
Matthieu <matthieu.fauchard2@gmail.com>
2025-12-03 13:04:05 +0100

    Cleaning step 2
    cleaned row rank 84

--- classics_cleaning.sql
+++ classics_cleaning.sql
@@ -1,7 +1,23 @@
-SELECT 
-  Rank as rank
-  ,Title as title
-  ,Author as author
-  ,LEFT(Year, 4) AS year
-FROM `book-project-479914.source_data.classics_list`
-ORDER BY Rank ASC;
+WITH Kafka AS (
+  SELECT
+    Rank
+    ,Title
+    ,LEFT(Author,11) AS Author
+    ,SUBSTRING(Author, 14, 4) AS Year
+  FROM `book-project-479914.source_data.classics_list`
+  WHERE rank = 84
+),
+
+Others AS (
+  SELECT
+    Rank
+    ,Title
+    ,Author
+    ,LEFT(Year,4) AS year
+  FROM `book-project-479914.source_data.classics_list`
+  WHERE rank <> 84
+)
+
+SELECT * FROM Kafka
+UNION ALL
+SELECT * FROM Others
