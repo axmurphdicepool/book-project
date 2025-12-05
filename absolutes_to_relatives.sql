1e2deca17efb2fe9670809fe3326fbe8be09dcb7
Matthieu <matthieu.fauchard2@gmail.com>
2025-12-05 16:01:47 +0100

    Update absolutes_to_relatives.sql
    dropped books from before 1900 to harmonize incrementation

--- absolutes_to_relatives.sql
+++ absolutes_to_relatives.sql
@@ -10,4 +10,5 @@ SELECT
   ,ROUND(nb_bestsellers/titles,2) AS percent_bestsellers
   ,ROUND(nb_classics/titles,2) AS percent_classics
   ,combined_years_on_bestellers_list
-FROM `book-project-479914.trial_and_error.m_dedup_classics_and_bestsellers_over_time` 
+FROM `book-project-479914.trial_and_error.m_dedup_classics_and_bestsellers_over_time`
+WHERE publication_decade IN ('1900s','1910s','1920s','1930s','1940s','1950s','1960s','1970s','1980s','1990s','2000s','2010s','2020s') 
