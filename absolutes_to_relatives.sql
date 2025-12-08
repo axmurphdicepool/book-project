17c0b7c578154bcd4e38b0b961f812c4a2d53f66
Matthieu <matthieu.fauchard2@gmail.com>
2025-12-08 10:34:38 +0100

    Update absolutes_to_relatives.sql
    added % of books with above average review score, num reviews & price

--- absolutes_to_relatives.sql
+++ absolutes_to_relatives.sql
@@ -1,3 +1,4 @@
+-- part 1, avg reviews per book, % of books with awards, in a series, bestsellers, classics
 SELECT  
   publication_decade
   ,titles
@@ -12,3 +13,33 @@ SELECT
   ,combined_years_on_bestellers_list
 FROM `book-project-479914.trial_and_error.m_dedup_classics_and_bestsellers_over_time`
 WHERE publication_decade IN ('1900s','1910s','1920s','1930s','1940s','1950s','1960s','1970s','1980s','1990s','2000s','2010s','2020s') 
+
+-- part 2, % of books above average review score, num reviews, price
+WITH global_avgs AS (
+  SELECT
+    AVG(review_score) AS avg_review_score,
+    AVG(reviews_count) AS avg_num_reviews,
+    AVG(price_eur) AS avg_price
+  FROM `book-project-479914.trial_and_error.m_dedup_pub_decade`
+)
+
+, decade_stats AS (
+  SELECT
+    publication_decade
+    ,AVG(CASE WHEN review_score > avg_review_score THEN 1 ELSE 0 END ) 
+      AS pct_above_avg_review_score
+    ,AVG( CASE WHEN reviews_count > avg_num_reviews THEN 1 ELSE 0 END ) 
+      AS pct_above_avg_num_reviews
+    ,AVG( CASE WHEN price_eur > avg_price THEN 1 ELSE 0 END ) 
+      AS pct_above_avg_price
+  FROM `book-project-479914.trial_and_error.m_dedup_pub_decade`, global_avgs
+  GROUP BY publication_decade
+)
+
+SELECT
+  *
+FROM book-project-479914.trial_and_error.m_dedup_relat AS part1
+LEFT JOIN decade_stats AS part2
+  ON part1.publication_decade = part2.publication_decade
+ORDER BY part1.publication_decade ASC
+
